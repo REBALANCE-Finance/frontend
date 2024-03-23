0e2be195bded03b303c09b9727d0dbd71df96338
Vasya Rymarenko <vrima60@gmail.com>
2024-03-23 18:35:50 +0200

    pages directory renamed

---
+++ src/pagesComponents/Pools/PoolsBorrow/index.tsx
@@ -0,0 +1,109 @@
+'use client'
+
+import { Box, Divider, Flex, HStack, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { observer } from "mobx-react-lite";
+import React, { useContext, useEffect } from "react";
+import { generatePath, useNavigate } from "react-router-dom";
+
+import { CardPool } from "../../../components/card";
+import { Tooltip } from "../../../components/tooltip";
+import { ROUTE_PATHS } from "../../../consts";
+import { BorrowButton } from "../../../features/actions/borrow-or-repay-button/BorrowButton";
+import { RepayButton } from "../../../features/actions/borrow-or-repay-button/RepayButton";
+import { storesContext } from "../../../store/app.store";
+import { formatNumber, formatPercent } from "../../../utils/formatNumber";
+import { IPoolData, IRowCard, RowCardNames, RowCardProccessType } from "../types";
+
+export const PoolsBorrow = observer(() => {
+  const { poolsStore } = useContext(storesContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    poolsStore.fetchPools("lending");
+  }, [poolsStore]);
+
+  const handleLink = (poolAddress: string) => {
+    navigate(generatePath(ROUTE_PATHS.borrowingAsset, { poolAddress }));
+  };
+
+  if (poolsStore.isLoading && poolsStore.pools.length === 0) {
+    return (
+      <Flex justifyContent="center" alignItems="center" h="100vh">
+        <Spinner />
+      </Flex>
+    );
+  }
+
+  if (poolsStore.error) {
+    return <Box textAlign="center">Error loading pools: {poolsStore.error.message}</Box>;
+  }
+
+  const rowCard: IRowCard[] = [
+    {
+      name: RowCardNames.header
+    },
+    {
+      name: RowCardNames.body,
+      proccess({ item, type }) {
+        switch (type) {
+          case RowCardProccessType.metrics:
+            return (
+              <>
+                <HStack justify="space-between">
+                  <Text color="black.0">Borrowing rate</Text>
+                  <Text textStyle="textMono16">{formatPercent(item.borrowRate)}</Text>
+                </HStack>
+
+                <HStack justify="space-between">
+                  <Text color="black.0">Total borrowed</Text>
+                  <Tooltip label="Total borrowed">
+                    <Text variant="tooltip" color="white" textStyle="textMono16">
+                      {formatNumber(item.borrowed)}
+                    </Text>
+                  </Tooltip>
+                </HStack>
+              </>
+            );
+          case RowCardProccessType.assets:
+            return (
+              <>
+                <Divider borderColor="black.60" />
+                <Flex alignItems="center" justifyContent="space-between">
+                  <Text fontSize="md" fontWeight="500" color="whiteAlpha.70">
+                    My Borrow
+                  </Text>
+                  <Text textStyle="textMono16">1000</Text>
+                </Flex>
+              </>
+            );
+          default:
+            return <></>;
+        }
+      }
+    },
+    {
+      name: RowCardNames.footer,
+      proccess({ item }) {
+        return (
+          <>
+            <BorrowButton pool={item} />
+            <RepayButton pool={item} />
+          </>
+        );
+      }
+    }
+  ];
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 3, xl: 4 }} spacing="24px">
+      {poolsStore.pools.map((pool: IPoolData) => (
+        <CardPool
+          key={pool.token}
+          itemCard={pool}
+          rowCard={rowCard}
+          onClick={() => handleLink(pool.rebalancerAddress)}
+        />
+      ))}
+    </SimpleGrid>
+  );
+});
---
+++ src/pagesComponents/Pools/PoolsLending/index.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import { Divider, Flex, HStack, SimpleGrid, Text } from "@chakra-ui/react";
+import { observer } from "mobx-react-lite";
+import React, { useContext, useEffect } from "react";
+import { generatePath, useNavigate } from "react-router-dom";
+import { useAccount } from "wagmi";
+
+import { CardPool } from "../../../components/card";
+import { Risk } from "../../../components/risk";
+import { Tooltip } from "../../../components/tooltip";
+import { ROUTE_PATHS } from "../../../consts";
+import { DepositLendingButton } from "../../../features/actions/deposit-or-withdraw-button/DepositLendingButton";
+import { WithdrawLendingButton } from "../../../features/actions/deposit-or-withdraw-button/WithdrawLendingButton";
+import { storesContext } from "../../../store/app.store";
+import { formatNumber, formatPercent } from "../../../utils/formatNumber";
+import { IRowCard, RowCardNames, RowCardProccessType } from "../types";
+import DepositInfo from "./components/DepositInfo";
+
+export const PoolsLending = observer(() => {
+  const { poolsStore } = useContext(storesContext);
+  const navigate = useNavigate();
+  const { address } = useAccount();
+  useEffect(() => {
+    poolsStore.fetchPools("lending");
+  }, [poolsStore]);
+
+  const handleLink = (poolAddress: string) => {
+    navigate(generatePath(ROUTE_PATHS.lendingAsset, { poolAddress }));
+  };
+  const rowCard: IRowCard[] = [
+    {
+      name: RowCardNames.header,
+      proccess({ item }) {
+        return (
+          <Flex direction="column" alignItems="center">
+            <Text fontFamily="Roboto mono" fontSize="xs" color="gray.100">
+              RISK
+            </Text>
+            <Risk risk={item.risk} />
+          </Flex>
+        );
+      }
+    },
+    {
+      name: RowCardNames.body,
+      proccess({ item, type }) {
+        switch (type) {
+          case RowCardProccessType.metrics:
+            return (
+              <>
+                <HStack justify="space-between">
+                  <Text color="white">Funds in pool</Text>
+                  <Tooltip label="Funds in pool">
+                    <Text variant="tooltip" textStyle="textMono16" color="white">
+                      {formatNumber(item.funds)} $
+                    </Text>
+                  </Tooltip>
+                </HStack>
+
+                <HStack justify="space-between">
+                  <Text color="white">Profit Earned</Text>
+                  <Text textStyle="textMono16">{formatNumber(item.earned)} $</Text>
+                </HStack>
+
+                <HStack justify="space-between">
+                  <Tooltip label="APR INFO">
+                    <Text color="white" variant="tooltip">
+                      30D avg. APR
+                    </Text>
+                  </Tooltip>
+                  <Text textStyle="textMono16">{formatPercent(item.avgApr)}</Text>
+                </HStack>
+
+                <HStack justify="space-between">
+                  <Text color="white">APR {">"} market avg.</Text>
+                  <Text color={item.apr > 0 ? "green.100" : "red"} textStyle="textMono16">
+                    {formatPercent(item.apr)}
+                  </Text>
+                </HStack>
+              </>
+            );
+          case RowCardProccessType.assets:
+            return (
+              <>
+                {!!address && item.token == "usdt" ? (
+                  <>
+                    <Divider borderColor="black.60" />
+                    <DepositInfo contractAddress={item.rebalancerAddress} ownerAddress={address} />
+                  </>
+                ) : null}
+              </>
+            );
+          default:
+            return <></>;
+        }
+      }
+    },
+    {
+      name: RowCardNames.footer,
+      proccess({ item }) {
+        return (
+          <>
+            <DepositLendingButton pool={item} />
+            <WithdrawLendingButton pool={item} />
+          </>
+        );
+      }
+    }
+  ];
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 3, xl: 4 }} spacing="24px">
+      {poolsStore.pools.map(elem => (
+        <CardPool
+          key={elem.token}
+          rowCard={rowCard}
+          itemCard={elem}
+          onClick={() => elem.token == "usdt" && handleLink(elem.rebalancerAddress)}
+        />
+      ))}
+    </SimpleGrid>
+  );
+});
