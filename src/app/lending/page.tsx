cb17b834755a02e4c97d6564ec0c826d42afda91
novgorodskii <novgorodskiji@gmail.com>
2024-06-13 19:56:12 +0300

    update main

--- src/app/lending/page.tsx
+++ src/app/lending/page.tsx
@@ -1,13 +1,49 @@
-import { getAreaChartAllIntervals, getChartData, getPools } from "@/api/pools/queries";
+'use client';
+import { useEffect, useState } from 'react';
+import { getAreaChartAllIntervals, getPools } from "@/api/pools/queries";
 import { PoolLayout } from "@/layout/PoolLayout";
 import { PoolsLending } from "@/pagesComponents/Pools/PoolsLending";
+import { IPoolData } from '@/api/pools/types';
 
-const LendingPage = async ({params} : {
-  params: { [key: string]: string }
-}) => {
-  const pools = await getPools('lending');
-  const token = pools?.find(item => item.rebalancerAddress === params.poolAddress)?.token || pools[0].token;
-  const chartData = await getAreaChartAllIntervals(token);
+const LendingPage = ({ params }: { params: { [key: string]: string } }) => {
+  const [pools, setPools] = useState<IPoolData[]>([]);
+  const [chartData, setChartData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedPools = await getPools('lending');
+        setPools(fetchedPools);
+        const token = fetchedPools?.find(item => item.rebalancerAddress === params.poolAddress)?.token || fetchedPools[0]?.token;
+        if (token) {
+          const fetchedChartData = await getAreaChartAllIntervals(token);
+          setChartData(fetchedChartData);
+        }
+        setLoading(false);
+      } catch (err) {
+        if (err instanceof Error) {
+          console.error("Ошибка при загрузке данных:", err);
+          setError(err.message);
+        } else {
+          console.error("Неизвестная ошибка:", err);
+          setError("Произошла неизвестная ошибка");
+        }
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [params.poolAddress]);
+
+  if (loading) {
+    return <div></div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <PoolLayout pools={pools} chartData={chartData}>
