6166c61d624b706027d5c03db091abb922802640
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2024-07-10 18:40:23 +0300

    minor ui warning block fix

--- src/components/warning-block/index.tsx
+++ src/components/warning-block/index.tsx
@@ -2,13 +2,7 @@ import { Flex, Text } from "@chakra-ui/react";
 
 const WarningBlock = () => {
   return (
-    <Flex
-      justify="center"
-      alignItems="center"
-      p="12px 16px"
-      borderBottom="1px solid lightGray"
-      bg="black.80"
-    >
+    <Flex justify="center" alignItems="center" p="12px 16px" bg="black.80">
       <Text color="lightGray" fontWeight={700}>
         Transition to Version 2.0: Withdrawals Only
       </Text>
