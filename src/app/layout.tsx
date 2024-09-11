6e1b2c700e2212de349b079f4689131972dff6e8
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2024-09-11 22:53:29 +0300

    added g-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,24 +8,39 @@ import { ModalContextController } from "../features/modals/ModalContextControlle
 import { ModalController } from "../features/modals/ModalController";
 import { ToastContainer } from "react-toastify";
 import { MainLayout } from "@/layout/MainLayout";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Rebalance",
-  description: "Rebalance",
+  description: "Rebalance"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}  
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-3F7178H2MV');
+          `
+          }}
+        />
+      </head>
       <body>
         <Providers>
-          <MainLayout>
-            {children}
-          </MainLayout>
+          <MainLayout>{children}</MainLayout>
           <ModalContextController />
           <ModalController />
           <ToastContainer
